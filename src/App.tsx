5587b2a70d95b04739bd1fcdfe8c7bbf10f20fdf
aiinkiestism <g.k.k.r.1228kc@gmail.com>
2023-03-17 00:05:08 +0900

    dev: add a few refs

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { useState } from 'react';
 import './App.css';
 import { WebGLText } from './components/ref/WebGLText';
+import { WebGLParticle } from './components/ref/WebGLParticle';
+import { WegGLPhisics } from './components/ref/WebGLPhisics';
 
 function App() {
   return (
     <div className="App">
-      <WebGLText />
+      {/* <WebGLText /> */}
+      {/* <WebGLParticle /> */}
+      <WegGLPhisics />
     </div>
   )
 }
